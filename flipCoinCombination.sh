828494770d6456e1d1b7ad956ea8e75fdb95499c
“PriyankaWadh” <wadhavanepriyanka@gmail.com>
2021-01-20 11:19:19 +0530

    Determine percent of Singlet comb.

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
-if [ $(( RANDOM%2 )) -eq 0 ]
-then
-            echo "Head"
-else
-            echo "Tail"
-fi
-
+declare -A coinflip
+coinflip=(["Head"]=0 ["Tail"]=0)
+for (( i=0; i<100; i++ ))
+do
+	if [ $(( RANDOM%2 )) -eq 0 ]
+	then
+        	    coinflip["Head"]=$(( ${coinflip[Head]}+1 ))
+	else
+        	    coinflip["Tail"]=$(( ${coinflip[Tail]}+1 ))
+	fi
+done
+echo "Percent of Head : "${coinflip[Head]}
+echo "Percent of Tail : "${coinflip[Tail]}
